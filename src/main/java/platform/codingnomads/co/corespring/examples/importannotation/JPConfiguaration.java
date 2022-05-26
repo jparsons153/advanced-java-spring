2fc191aef0f8271a6fe1b7ed1e17073dfba01ba5
jparsons153 <jparsons153@hotmail.com>
2022-05-26 09:56:48 +0100

    import annonation lab

--- src/main/java/platform/codingnomads/co/corespring/examples/importannotation/JPConfiguaration.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/importannotation/JPConfiguaration.java
@@ -2,16 +2,10 @@ package platform.codingnomads.co.corespring.examples.importannotation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({SimpleConfiguration.class, JPConfiguaration.class})
-public class ImportAnnotationConfig {
-    @Bean
-    public Framework framework() {
-        return new Framework();
-    }
 
+public class JPConfiguaration {
     @Bean
     public Framework frame() {
         return new Framework();
