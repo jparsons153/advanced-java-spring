f87b004ef9ed64a1aba24703a815214ed30ec455
jparsons153 <jparsons153@hotmail.com>
2022-05-29 08:28:00 +0100

    spring jdbc lab

--- src/main/java/platform/codingnomads/co/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/springdatajdbc/SpringDataJDBCDemo.java
@@ -22,7 +22,7 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
 
     @Override
     public void run(String... strings) {
-
+/*
         try {
             //create employee table using the JdbcTemplate method "execute"
             jdbcTemplate.execute("CREATE TABLE employees (id BIGINT AUTO_INCREMENT PRIMARY KEY," +
@@ -53,5 +53,39 @@ public class SpringDataJDBCDemo implements CommandLineRunner {
         jdbcTemplate.execute("TRUNCATE TABLE employees;");
         //delete the table
         jdbcTemplate.execute("DROP TABLE employees");
+*/
+        // NEW POJO
+
+        try {
+            //create address table using the JdbcTemplate method "execute"
+            jdbcTemplate.execute("CREATE TABLE address (id BIGINT AUTO_INCREMENT PRIMARY KEY," +
+                    "streetName VARCHAR(255) NOT NULL, postCode VARCHAR(255) NOT NULL );");
+        } catch (Exception e) {
+            //nothing
+        }
+
+        //create a list of first and last names
+        List<Object[]> splitAddress = Stream.of("hyperbole loop, AB11CDE")
+                .map(address -> address.split(", "))
+                .collect(Collectors.toList());
+
+        //for each first & last name pair insert an Address into the database
+        for (Object[] address : splitAddress) {
+            jdbcTemplate.execute(String.format("INSERT INTO address(id, streetName, postCode) VALUES ('%s','%s','%s')", address[0], address[1], address[2]));
+        }
+
+        //query the database for Address
+        jdbcTemplate.query(
+                        "SELECT id, streetName, postcode FROM address WHERE streetName = 'hyperbole loop'",
+                        (rs, rowNum) -> new Address(rs.getLong("id"), rs.getString("streetName"), rs.getString("postCode"))
+                )
+                //print each found address to the console
+                .forEach(address -> System.out.println(address.toString()));
+
+        //truncate the table
+        jdbcTemplate.execute("TRUNCATE TABLE address;");
+        //delete the table
+        jdbcTemplate.execute("DROP TABLE address");
+
     }
 }
