7096beaa021e72a1bfe3289c976894f7c6bfb0ba
jparsons153 <jparsons153@hotmail.com>
2022-06-05 18:22:38 +0100

    crud repo lab

--- src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
@@ -23,21 +23,32 @@ public class CrudRepoDemo implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //create new user
-        User user = User.builder().firstName("Bobby").lastName("Bobbert").age(56).build();
+        User user1 = User.builder().firstName("Bobby").lastName("Bobbert").age(56).build();
         User user2 = User.builder().firstName("Joanne").lastName("Joanna").age(36).build();
+        User joeBloggs = User.builder().firstName("Joe").lastName("Bloggs").age(21).build();
+        User chuckNorris = User.builder().firstName("Chuck").lastName("Norris").age(45).build();
+
+        // add users to arraylist
+        ArrayList<User> newUsers = new ArrayList<>();
+        newUsers.add(user1);
+        newUsers.add(user2);
+        newUsers.add(joeBloggs);
+        newUsers.add(chuckNorris);
+
+        Iterable<User> users = userRepo.saveAll(newUsers);
 
         //save user and assign what is returned to the user variable.
-        user = userRepo.save(user);
-        user2 = userRepo.save(user2);
+        //user = userRepo.save(user);
+        //user2 = userRepo.save(user2);
 
-        Iterable<User> users = userRepo.findAll();
+        Iterable<User> u = userRepo.findAll();
 
-        for(User u : users){
-            System.out.println(u.toString());
+        for(User user : users){
+            System.out.println(user.toString());
         }
 
         //delete the user using the id of the inserted user object
-        userRepo.deleteById(user.getId());
-        userRepo.deleteById(user2.getId());
+        //userRepo.deleteById(user.getId());
+        //userRepo.deleteById(user2.getId());
     }
 }
