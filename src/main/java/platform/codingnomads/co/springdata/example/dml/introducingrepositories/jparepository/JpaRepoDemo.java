47130669aa2d613279955468a994eb531aee1226
jparsons153 <jparsons153@hotmail.com>
2022-06-07 07:11:43 +0100

    Soft drink lab

--- src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/jparepository/JpaRepoDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/jparepository/JpaRepoDemo.java
@@ -23,32 +23,40 @@ public class JpaRepoDemo implements CommandLineRunner {
         SoftDrink fanta = SoftDrink.builder().name("Fanta").rating(10).build();
         SoftDrink coke = SoftDrink.builder().name("Coca-Cola").rating(4).build();
         SoftDrink drPepper = SoftDrink.builder().name("Dr. Pepper").rating(1).build();
+        SoftDrink fantaLemon = SoftDrink.builder().name("Fanta_Lemon").rating(2).build();
+        SoftDrink seven_up = SoftDrink.builder().name("7up").rating(5).build();
+
 
         //save single entity instance
-        fanta = softDrinkRepo.save(fanta);
+       // fanta = softDrinkRepo.save(fanta);
+
+        // update rating for dr.pepper and coke
+        drPepper.setRating(6);
+        coke.setRating(5);
 
         //save multiple entity instances at a time
-        List<SoftDrink> insertedSoftDrinks = softDrinkRepo.saveAll(List.of(coke, drPepper));
+        List<SoftDrink> insertedSoftDrinks = softDrinkRepo.saveAll(List.of(fanta, coke, drPepper, fantaLemon, seven_up));
 
         //make sure all entities are actually saved to the database
         softDrinkRepo.flush();
 
         //update coke and drPepper to have rating 0 in the database
-        for (SoftDrink sd : insertedSoftDrinks) {
-            sd.setRating(0);
-            softDrinkRepo.save(sd);
-        }
+       // for (SoftDrink sd : insertedSoftDrinks) {
+       //     sd.setRating(0);
+       //     softDrinkRepo.save(sd);
+       // }
+
 
         System.out.println("ALL SOFT DRINKS IN DESCENDING ORDER BASED ON ID");
         //get all soft drinks in ascending order and print toString() to the console
         softDrinkRepo.findAll(Sort.by(Sort.Direction.DESC, "id")).forEach(System.out::println);
 
-        //find all using an example
-        System.out.println("FINDING ALL USING EXAMPLE");
+        //find all with a rating of 5
+        System.out.println("FINDING ALL WITH RATING OF 5");
         softDrinkRepo.findAll(
                         Example.of(
                                 //probe soft drink to match results with
-                                SoftDrink.builder().rating(0).build(),
+                                SoftDrink.builder().rating(5).build(),
                                 //ask that database entries that match any of the fields in the probe be returned
                                 ExampleMatcher.matchingAny())
                 )
