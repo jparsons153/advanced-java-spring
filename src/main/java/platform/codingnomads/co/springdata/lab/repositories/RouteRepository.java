648186252f7d00d5f824acac3d5eb7006fd664a9
jparsons153 <jparsons153@hotmail.com>
2022-07-07 20:21:31 +0100

    update Route repo to include route as input

--- src/main/java/platform/codingnomads/co/springdata/lab/repositories/RouteRepository.java
+++ src/main/java/platform/codingnomads/co/springdata/lab/repositories/RouteRepository.java
@@ -3,6 +3,7 @@ package platform.codingnomads.co.springdata.lab.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import platform.codingnomads.co.springdata.lab.models.Area;
+import platform.codingnomads.co.springdata.lab.models.Route;
 
-public interface RouteRepository extends JpaRepository<Area,Long> {
+public interface RouteRepository extends JpaRepository<Route,Long> {
 }
