38b090520d6f17f174918e30e9fc52c68268deaf
jparsons153 <jparsons153@hotmail.com>
2022-07-13 09:29:05 +0100

    added method to return list using ResponseBody

--- src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/responsebody/controller/UserController.java
+++ src/main/java/platform/codingnomads/co/springweb/returningdatatoclient/responsebody/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import platform.codingnomads.co.springweb.returningdatatoclient.responsebody.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
 
@@ -14,6 +17,24 @@ public class UserController {
             .id(1000)
             .name("Spring Dev")
             .email("dev@codingnomads.co")
+            .build();
+
+    public User user2 = User.builder()
+            .id(101)
+            .name("Java Vanilla")
+            .email("vanilla@codingnomads.co")
+            .build();
+
+    public User user3 = User.builder()
+            .id(102)
+            .name("user 03")
+            .email("user03@codingnomads.co")
+            .build();
+
+    public User user4 = User.builder()
+            .id(103)
+            .name("user 04")
+            .email("user04@codingnomads.co")
             .build();
 
     //using ResponseBody to return a POJO
@@ -34,4 +55,16 @@ public class UserController {
     public User user() {
         return user;
     }
+
+    @ResponseBody
+    @GetMapping("/user-list")
+    public List<User> userListResponseBody() {
+        List<User> userList = new ArrayList<>();
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+
+    return userList;
+    }
+
 }
